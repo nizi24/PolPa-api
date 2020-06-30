c7fdb01202e08e4047ef0d4ec237be8b43ad0f67
nizi <failaforcontact@gmail.com>
2020-06-30 11:51:16 +0900

    add model required_exp

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_26_100055) do
+ActiveRecord::Schema.define(version: 2020_06_30_023523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "required_exps", force: :cascade do |t|
+    t.integer "level", null: false
+    t.integer "required_exp", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["level"], name: "index_required_exps_on_level", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
---
+++ test/models/required_exp_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RequiredExpTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
