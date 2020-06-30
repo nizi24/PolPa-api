11e3f517bafa54ee79a441a1376be52866b616d2
nizi <failaforcontact@gmail.com>
2020-06-30 11:55:22 +0900

    add required_exp_seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 common_table_name = %w(required_exp)
 common_table_name.each do |table_name|
-  path = Rails.root.join('db', 'seeds', "#{tabel_name}.rb")
+  path = Rails.root.join('db', 'seeds', "#{table_name}.rb")
   if File.exist?(path)
-    puts "Creating #{tabel_name}....."
+    puts "Creating #{table_name}....."
     require(path)
   end
 end
