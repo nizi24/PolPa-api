5605f5b04e954d5f49924b24b85e41d03a158da2
nizi <failaforcontact@gmail.com>
2020-08-01 09:00:56 +0900

    corsの設定を変更

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    if Rails.env.production?
+        origins 'https://studylevel-47929.firebaseapp.com'
+    else
+        origins 'http://localhost:8080'
+    end
 
     resource '*',
       headers: :any,
