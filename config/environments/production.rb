1cd62cfd8b6e02982cca93193cfd06d96b38018b
nizi <failaforcontact@gmail.com>
2020-08-02 18:06:45 +0900

    [add] production.rb を変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,12 +86,14 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'polpa-v1-api.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
+  user_name = ENV['SENDGRID_USERNAME']
+  pass = ENV['SENDGRID_PASSWORD']
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => user_name,
+    :password       => pass,
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
